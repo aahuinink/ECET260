f7eb227bac468f8ed4c527000100213529b57323
AaronH_MSI <a_huinink@hotmail.ca>
2023-09-28 14:49:21 -0700

    basic project setup, printf defined

---
+++ labs/adcInt/Core/Inc/thermistor.h
@@ -0,0 +1,16 @@
+/*
+ * thermistor.h
+ *
+ *  Created on: Sep 21, 2023
+ *      Author: a_hui
+ */
+
+#ifndef INC_THERMISTOR_H_
+#define INC_THERMISTOR_H_
+
+#include <math.h>
+#include <stdint.h>
+
+float convertAnalogToTemperature(uint32_t AnalogReadValue);
+
+#endif /* INC_THERMISTOR_H_ */
