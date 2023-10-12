76cf70a1578aa6f6e127759633aa5104a2addf0b
AaronH_MSI <a_huinink@hotmail.ca>
2023-10-12 15:08:24 -0700

    fixed ADC

--- labs/adcInt/Core/Src/main.c
+++ labs/adcInt/Core/Src/main.c
@@ -266,6 +266,7 @@ static void MX_ADC1_Init(void)
 
   /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
   */
+  sConfig.Channel = ADC_CHANNEL_1;
   sConfig.Rank = 3;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
